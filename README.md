559380bf297d784fe4afc21ccface2df60305ecf
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-03-21 15:58:09 +1000

    Add Vector Math starter code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cp3408_prac_04
 CP3408 Game Engines and Simulation - Practical 04 in Unity
+
+Introduction to AI
---
+++ VectorMath/Assets/Move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+
+    Vector3 direction;
+    public float speed = 2f;
+
+    void Start() {
+
+        //direction = goal.transform.position - this.transform.position;
+        //this.transofrm.Translate(direction);
+    }
+
+    private void LateUpdate() {
+        
+        direction = goal.transform.position - this.transform.position;
+        // Make the character look at their goal
+        this.transform.LookAt(goal.transform.position);
+        // Stop within a threshold value to goal, using magnitude checking will allow to follow goal
+        if (direction.magnitude > 2)
+        {
+            // Normalise scales the vector to set units, deltaTime ensures it runs consistently
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+    }
+}
